3ed4547b0e539d579ff22d743a70ce6acf9f1c5b
David Schissler <david.schissler@gmail.com>
2025-11-06 05:17:24 +0200

    Optimize Ucom.

--- src/ucom/Ucom.ts
+++ src/ucom/Ucom.ts
@@ -5,9 +5,13 @@ import plugMan from './plugman.ts'
 import comMan from './comman.ts'
 
 export default (pluginClasses: PluginConstructor[] = []) => {
-  let hasStarted = false
   const plugins = plugMan(pluginClasses)
   const man = comMan(plugins)
+  
+  let hasStarted = false
+  const checkStarted = () => {
+    if (!hasStarted) { throw Error('start before defining') }
+  }
 
   return {
     async start() {
@@ -16,31 +20,18 @@ export default (pluginClasses: PluginConstructor[] = []) => {
       }
       hasStarted = true
 
-      const start = () => plugins.start({man})
-      return (async () => {
-        if (document.readyState === 'loading') {
-          await new Promise(resolve => {
-            document.addEventListener('DOMContentLoaded', resolve)
-          })
-        }
-        await start()
-      })()
-    },
-    resolve(...params: [url: string]) {
-      return man.resolve(...params)
-    },
-    async define(...params: [name: string | null, tpl: HTMLTemplateElement]) {
-      if (!hasStarted) {
-        throw Error('start before defining')
+      if (document.readyState === 'loading') {
+        await new Promise(resolve => document.addEventListener('DOMContentLoaded', resolve))
       }
-
+      return plugins.start({man})
+    },
+    resolve: man.resolve,
+    async define(...params: [name: string | null, tpl: HTMLTemplateElement]) {
+      checkStarted()
       return man.define(...params)
     },
     async import(...params: [url: string, tpl?: HTMLTemplateElement]) {
-      if (!hasStarted) {
-        throw Error('start before defining')
-      }
-
+      checkStarted()
       return man.import(...params)
     },
   }
