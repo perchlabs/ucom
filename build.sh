bfdf8f9ac17d4ad0772cc7ffa374a54267ee0ad1
David Schissler <david.schissler@gmail.com>
2025-12-07 06:32:06 +0200

    Add bun build.

--- build.sh
+++ build.sh
@@ -3,8 +3,37 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 SRC_DIR="$ROOT_DIR/src"
 OUT_DIR="$ROOT_DIR/dist"
 
-for name in "$@"; do
-  ENTRY="$name.ts"
-  esbuild --format=esm --out-extension:.js=.js --minify --bundle "$SRC_DIR/$ENTRY" --outdir="$OUT_DIR"
-#  esbuild --format=esm --out-extension:.js=.js --bundle "$SRC_DIR/$ENTRY" --outdir="$OUT_DIR"
+UTIL="$1"
+
+if [ "$#" -eq 0 ]; then
+  echo "You must choose a build utility '$UTIL'." >&2
+  exit 1
+fi
+if [ "$#" -eq 1 ]; then
+  echo "You must choose a entry name." >&2
+  exit 1
+fi
+
+for name in "${@:2}"; do
+  INPUT="$name.ts"
+  ENTRY="$SRC_DIR/$INPUT"
+
+  if [ ! -f "$ENTRY" ]; then
+    echo "Entry file '$ENTRY' does not exist."
+    exit 1
+  fi
+
+  case "$UTIL" in
+  esbuild)
+    esbuild --format=esm --out-extension:.js=.js --minify --bundle \
+      "$ENTRY" --outdir="$OUT_DIR"
+  ;;
+  bun)
+    bun build --production --outdir="$OUT_DIR" "$ENTRY"
+  ;;
+  *)
+    echo "Unknown build utility." >&2
+    exit 1
+  ;;
+  esac
 done
