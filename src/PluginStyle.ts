b04b8d9270556fbd71ce281d9bd0ee70cb094ed0
David Schissler <david.schissler@gmail.com>
2026-04-12 04:44:00 +0300

    Small cleanup of style plugin.

--- src/PluginStyle.ts
+++ src/PluginStyle.ts
@@ -30,41 +30,40 @@ export default class implements Plugin {
   }
 
   #getSheetsLoader(): Promise<CSSStyleSheet[]> {
-    if (!this.#sheetsLoader) {
-      this.#sheetsLoader = this.#loadSheets()
-    }
+    this.#sheetsLoader ??= loadSheets()
     return this.#sheetsLoader
-  }
-
-  // TODO: Revisit this when Safari and Firefox support CSS imports "with {type: 'css'}".
-  // Note: Constructed stylesheet asynchronous replace method no longer handles CSS @import rules as 2025.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import/with#browser_compatibility
-  async #loadSheets(): Promise<CSSStyleSheet[]> {
-    const loading = queryForStyles(document.head)
-      .map(el => {
-        return new Promise((resolve, reject) => {
-          if (el.sheet) {
-            return resolve(el.sheet)
-          }
-          el.addEventListener('load', () => resolve(el.sheet), false)
-          el.addEventListener('error', () => reject(new Error('theme failed loading')), false)
-        })
-      })
-      .filter(v => Boolean(v))
-
-    return (await Promise.allSettled(loading))
-      .filter(v => v?.status === 'fulfilled')
-      .map(v => v.value as CSSStyleSheet)
-      .map((domSheet: CSSStyleSheet) => {
-        // Shadowdom can only use constructable style sheets.
-        const sheet = new CSSStyleSheet()
-        sheet.replaceSync(ArrayFrom(domSheet.cssRules).map(v => v.cssText).join(' '))
-        return sheet
-      })
   }
 }
 
+// TODO: Revisit this when Safari and Firefox support CSS imports "with {type: 'css'}".
+// Note: Constructed stylesheet asynchronous replace method no longer handles CSS @import rules as 2025.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import/with#browser_compatibility
+async function loadSheets(): Promise<CSSStyleSheet[]> {
+  const loading = queryForStyles(document.head)
+    .map(el => {
+      return new Promise((resolve, reject) => {
+        if (el.sheet) {
+          return resolve(el.sheet)
+        }
+        el.addEventListener('load', () => resolve(el.sheet), false)
+        el.addEventListener('error', () => reject(new Error('theme failed loading')), false)
+      })
+    })
+    .filter(v => Boolean(v))
+
+  return (await Promise.allSettled(loading))
+    .filter(v => v?.status === 'fulfilled')
+    .map(v => v.value as CSSStyleSheet)
+    .map((domSheet: CSSStyleSheet) => {
+      // Shadowdom can only use constructable style sheets.
+      const sheet = new CSSStyleSheet()
+      sheet.replaceSync(ArrayFrom(domSheet.cssRules).map(v => v.cssText).join(' '))
+      return sheet
+    })
+}
+
+
 function queryForStyles(root: QueryableRoot) {
-  const selector = ['LINK', 'STYLE'].map(k => `${k}[${ATTR_CORE}]`).join(',')
+  const selector = ['STYLE', 'LINK'].map(k => `${k}[${ATTR_CORE}]`).join(',')
   return queryAll<HTMLStyleElement | HTMLLinkElement>(root, selector)
 }
